e6906e8e4c898c8151f41932980af94bc4fba223
Vinicius <viniciusaagostini@gmail.com>
2018-05-29 00:11:46 -0300

    create get matches route and implement business logic

--- server/helpers/ErrorMessages.js
+++ server/helpers/ErrorMessages.js
@@ -6,6 +6,9 @@ module.exports = {
   ERROR_ON_FOLLOW_USER: 'Não foi possível seguir o usuário.',
   ERROR_ON_UNFOLLOW_USER: 'Não foi possível deixar de seguir o usuário.',
 
+  //match
+  ERROR_RETRIEVING_MATCHES: 'Algo inesperado ocorreu ao recuperar as partidas.',
+
   //auth
   INVALID_EMAIL_OR_PASSWORD: 'Email ou senha incorretos.',
   FORBIDDEN_DEFAULT: 'Desculpe, você não pode fazer isso.'
